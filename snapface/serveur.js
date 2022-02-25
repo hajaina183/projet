db30d0427e153a710acec66534d75dfdbde54584
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-02-25 21:31:46 +0300

    Création du serveur avec Express

--- snapface/serveur.js
+++ snapface/serveur.js
@@ -1,12 +1,10 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname + '/dist/snapface'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/snapface/index.html'));
-})
-
-app.listen(process.env.PORT);
+     res.sendFile(path.join(__dirname+'/dist/snapface/index.html'));
+});
