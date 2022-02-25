632106d6aeea9457f33c5ef7f52a7ff433005840
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-02-25 21:19:32 +0300

    commit pret

---
+++ snapface/serveur.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/snapface'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/snapface/index.html'));
+})
+
+app.listen(process.env.PORT);
