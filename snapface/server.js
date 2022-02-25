8c3ef91a0736a5516cfaa9b787595b1c6763223d
hajaina183 <80822797+hajaina183@users.noreply.github.com>
2022-02-25 22:06:36 +0300

    config4

--- snapface/server.js
+++ snapface/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/snapface'));
 
 app.get('/*', function(req,res) {
-     res.sendFile(path.join(__dirname+'/dist/snapface/index.html'));
+     res.sendFile(path.join(__dirname+'/dist/snapface/','index.html'));
 });
 
 app.listen(process.env.PORT);
